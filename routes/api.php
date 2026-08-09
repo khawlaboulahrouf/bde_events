e1fdcdaeed16c2ed581d64e82f0f80e986b5dc8b
Boulahrouf_Khaoula <khawlaboulahrouf@gmail.com>
2026-08-09 21:30:24 +0100

    travallier sur authorization

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthApiController;
 
-
 Route::post('/login', [AuthApiController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+
+    Route::get('/admin/test', function (Request $request) {
+        return response()->json([
+            'message' => 'Bienvenue Admin !'
+        ]);
+    });
+
+});
